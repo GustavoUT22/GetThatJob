f1e287373e62cad70f9e148ef39ff20e67611f30
Gustavout22 <gustavougartetorres@gmail.com>
2023-07-17 14:55:54 -0500

    add: gloabl styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import './index.css'
+import { Global } from "@emotion/react";
+import { reset } from "./styles/reset.js";
+import { global } from "./styles/global.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Global styles={reset} />
+    <Global styles={global} />
     <App />
   </React.StrictMode>
 );
