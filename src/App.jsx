f1730ebc22bc3e7a8010635f10092d492960c6a2
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-17 16:14:24 -0500

    add: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 // import "./App.css";
+import SideBar from "./components/SideBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Get That Job</h1>
+      <SideBar />
     </>
   );
 }
