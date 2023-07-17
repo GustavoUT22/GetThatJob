ef1385c027de7bb4219e92028aa4b20407b9f398
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-07-17 13:48:17 -0600

    chore: add styles

---
+++ src/styles/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+  gray: {
+    content: "#1E1E1E",
+    content_secondary: "#5C5C5C",
+    dark: "#373737",
+    gray: "#616161",
+    light: "#8E8E8E",
+    bg_dark: "#E1E2E1",
+    bg_light: "#F5F5F6",
+  },
+
+  pink: {
+    dark: "#BF5F82",
+    pink: "#F48FB1",
+    light: "#FFC1E3",
+  },
+
+  black: "#171717",
+  white: "#FFFFFF",
+};
