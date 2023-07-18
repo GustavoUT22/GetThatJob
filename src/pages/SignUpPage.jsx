f7336a841fa26efd8e6b9011dc1b10bc0a46b0f0
FMondragon7 <fernandomondragon07@gmail.com>
2023-07-18 15:01:11 -0700

    feat: add signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import styled from "@emotion/styled";
 
-const SignUpPage = () => {
-  return <div></div>;
-};
+import { colors } from "../styles";
+import Header from "../components/Header";
+import SignupForm from "../components/SignupForm";
 
-export default SignUpPage;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px 0;
+  gap: 8px;
+`
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: row;
+  width: auto;
+  height: 92vh;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 17px 0;
+  background-color: ${colors.gray.bg_dark}
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  padding: 5px 0;
+  gap: 18px;
+`
+
+const Title = styled.h1`
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const Paragraph = styled.p`
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28px; /* 140% */
+  letter-spacing: 0.15px;
+`
+
+
+export default function SignUpPage() {
+  
+  return (
+    <>
+      <Container>
+        <Header/>
+        <Main>
+          <Section>
+            <Title>Good Choice!</Title>
+            <Paragraph>Create a new account as...</Paragraph>
+            <SignupForm /> 
+          </Section>
+        </Main>
+      </Container>
+    </>
+  )
+}
+
