b987b642bfe1a4749c0ca5ff6a371fe8af2b295d
FMondragon7 <fernandomondragon07@gmail.com>
2023-07-19 23:50:42 -0700

    refactor: signupform refactor for map information

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 
 import { colors } from "../styles";
-import step1 from "../assets/images/step1.svg"
-import step2 from "../assets/images/step2.svg"
-import step3 from "../assets/images/step3.svg"
+import current1 from "../assets/images/current1.svg"
+import done1 from "../assets/images/done1.svg"
+import done2 from "../assets/images/done2.svg"
+import pending2 from "../assets/images/pending2.svg"
+import current2 from "../assets/images/current2.svg"
+import current3 from "../assets/images/current3.svg"
+import pending3 from "../assets/images/pending3.svg"
 import RecruiterForm from "./RecruiterForm";
+import ProfessionalForm from "./ProfessionalForm";
 
 
 const RoleSection = styled.div`
@@ -59,6 +64,23 @@ const Rectangle = styled.div`
   align-self: stretch;
 `
 
+const StatusLabel = styled.label`
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+`
+
+const InfoLabel = styled.label`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0.5px;
+`
+
 export default function SignupForm() {
   function handleRoleChange(event) {
     setSelectedRole(event.target.value)
@@ -71,16 +93,16 @@ export default function SignupForm() {
     1: "Pending",
     2: "Pending",
   })
-  
+
   const stepsInfo = {
     "professional": [
-      {img: step1, status: status[0], label: "Login Information"}, 
-      {img: step2, status: status[1], label: "Personal Information"}, 
-      {img: step3, status: status[2], label: "Professional Information"}
+      {img: {1: current1, 2: done1, 3: done1 }, status: status[0], label: "Login Information"}, 
+      {img: {1: pending2, 2: current2, 3: done2}, status: status[1], label: "Personal Information"}, 
+      {img: {1: pending3, 2: pending3, 3: current3}, status: status[2], label: "Professional Information"}
     ],
     "recruiter": [
-      {img: step1, status: status[0], label: "Login Information"}, 
-      {img: step2, status: status[1], label: "Company Information"}
+      {img: {1: current1, 2: done1} , status: status[0], label: "Login Information"}, 
+      {img: {1: pending2, 2: current2}, status: status[1], label: "Company Information"}
     ]
   }
 
@@ -110,15 +132,15 @@ export default function SignupForm() {
       <Steps>
         {stepsInfo[selectedRole].map((info, index) => (
           <Step key={index}>
-            <img src={info.img} alt={info.label + " icon"}/>
-            <StepInfo>
-              <label>{info.status}</label>
-              <label>{info.label}</label>
+            <img src={info.img[status.step]} alt={info.label + " icon"}/>
+            <StepInfo style={{opacity: info.status === "Pending" ? "50%" : "1"}}>
+              <StatusLabel>{info.status}</StatusLabel>
+              <InfoLabel>{info.label}</InfoLabel>
             </StepInfo>
           </Step>
         ))}
       </Steps>
-      {selectedRole === "recruiter" ? <RecruiterForm step={status.step} setStatus={setStatus}/> : <p>hola</p>}
+      {selectedRole === "recruiter" ? <RecruiterForm step={status.step} setStatus={setStatus}/> : <ProfessionalForm step={status.step} setStatus={setStatus}/>}
     </>
   )
 }
